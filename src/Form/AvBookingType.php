c602a600a9dddbee2c59f8cefb253ac08ae96e55
Tonie <tonie.lanquette.simplon@gmail.com>
2024-05-14 11:24:06 +0200

    style missing booking, save avant correction BDD

--- src/Form/AvBookingType.php
+++ src/Form/AvBookingType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use App\Entity\AvBooking;
+use App\Entity\AvForms;
+use App\Entity\AvStatus;
+use App\Entity\AvTravels;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,6 +16,14 @@ class AvBookingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+        ->add('avTravels', EntityType::class, [
+            'class' => AvTravels::class,
+            'choice_label' => 'name',    
+        ])
+        ->add('avStatus', EntityType::class, [
+            'class' => AvStatus::class,
+            'choice_label' => 'name',
+        ])
         ;
     }
 
